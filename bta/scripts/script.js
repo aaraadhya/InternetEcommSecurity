25599c7692d61f587d8368ed3c6c5602e678a3c7
aanarra <aanarra@cisco.com>
2015-05-14 15:46:17 -0700

    Made some changes and added navigation

---
+++ bta/scripts/script.js
@@ -0,0 +1,10 @@
+$('#submit').click(function () {
+    window.location.href = "pass.html";
+    return false;
+ });
+$('#submitPass').click(function () {
+    window.location.href= "catalogue.html";
+    return false;
+});
+
+
